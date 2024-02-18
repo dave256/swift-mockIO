89d53b272e046df2ce243f6536de0f9cd254d45b
Dave Reed <dave256@mac.com>
2024-02-18 09:34:12 -0500

    Package.swift update

--- Package.swift
+++ Package.swift
@@ -4,7 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "MockIO",
+    name: "swift-mockIO",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
